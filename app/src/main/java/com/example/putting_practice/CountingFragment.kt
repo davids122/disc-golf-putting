4b1821d966544692a81844e90af0f9883da05908
David Sprague <dsprague@unc.edu>
2024-02-21 12:43:10 -0500

    Add functionality to count putts

---
+++ app/src/main/java/com/example/putting_practice/CountingFragment.kt
@@ -0,0 +1,86 @@
+package com.example.putting_practice
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
+import com.example.putting_practice.databinding.FragmentCountingBinding
+import com.google.android.material.snackbar.Snackbar
+
+class CountingFragment : Fragment() {
+
+    private var num_shots_View: TextView? = null
+    // TODO: Rename and change types of parameters
+    private var numShotsRecieved = ""
+    private var numShotsMade = 0
+    private var numShotsMissed = 0
+    private var numShotsRemaining: Int = 5
+        set(value) {
+            field = value
+            "$value putts remaining".also { num_shots_View?.text = it }
+        }
+
+
+    private var _binding: FragmentCountingBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setFragmentResultListener("requestKey") { _, bundle ->
+            numShotsRecieved = bundle.getString("bundleKey") ?: "5"
+            numShotsRemaining = numShotsRecieved.toInt()
+            "$numShotsRemaining putts remaining!".also { num_shots_View?.text = it }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentCountingBinding.inflate(inflater, container, false)
+
+        num_shots_View = binding.numShotsText
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.madeShotButton.setOnClickListener {
+            if(numShotsRemaining > 0) {
+                numShotsMade += 1
+                numShotsRemaining -= 1
+                if(numShotsRemaining == 0){
+                    this.view?.let {
+                        Snackbar.make(it, "made:$numShotsMade miss:$numShotsMissed", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show()
+                    }
+                }
+            }
+        }
+
+        binding.missedShotButton.setOnClickListener {
+            if(numShotsRemaining > 0){
+                numShotsMissed += 1
+                numShotsRemaining -= 1
+                if(numShotsRemaining == 0){
+                    this.view?.let {
+                        Snackbar.make(it, "made:$numShotsMade miss:$numShotsMissed", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/putting_practice/SecondFragment.kt
@@ -0,0 +1,84 @@
+package com.example.putting_practice
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
+import androidx.navigation.fragment.findNavController
+import com.example.putting_practice.databinding.FragmentSecondBinding
+import com.google.android.material.snackbar.Snackbar
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment(), AdapterView.OnItemSelectedListener{
+
+    var num_shots_selected = ""
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSecond.setOnClickListener {
+            val result = num_shots_selected
+            // Use the Kotlin extension in the fragment-ktx artifact.
+            setFragmentResult("requestKey", bundleOf("bundleKey" to result))
+            findNavController().navigate(R.id.action_SecondFragment_to_CountingFragment)
+        }
+        
+        // access the num_shots spinner
+        val spinner: Spinner = view.findViewById(R.id.num_shots_spinner)
+        spinner.onItemSelectedListener = this
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter.createFromResource(
+            this.requireContext(),
+            R.array.number_of_shots,
+            android.R.layout.simple_spinner_item
+
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner.
+            spinner.adapter = adapter
+        }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        num_shots_selected = parent.getItemAtPosition(pos).toString()
+        this.view?.let {
+            Snackbar.make(it, num_shots_selected, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        //do nothing
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
